871c8faafe54adfdb4805895d8032b28a3a03125
David Tingley <davidtingley2@gmail.com>
2021-11-10 17:06:15 +0100

    Update merge_sbx.m

--- +pipe/+io/merge_sbx.m
+++ +pipe/+io/merge_sbx.m
@@ -5,6 +5,7 @@ for f = 1:length(fileList)
        info = pipe.io.read_sbxinfo([meta{1} '.mat']);
        temp = pipe.io.read_sbx(fileList(f).name);
        
+       % hacky code that assumes file naming ANIMAL_DATE_RUN.sbx
        t = strsplit(meta{1},'_');
        animal = t{1};
        date = t{2};
@@ -23,16 +24,17 @@ for f = 1:length(fileList)
        nFrames(f) = size(temp,ndims(temp));
        nstacks(f) = completeStacks;
 
-       if r == 1
+       info.nframes = sum(nFrames); % needs to be updated for regWriter, dumb
+       % write new if first file, otherwise append
+       if f == 1
         rw = pipe.io.RegWriter([mergeFile '.sbx'], info, 'sbx', true,'w');      
        else
         rw = pipe.io.RegWriter([mergeFile '.sbx'], info, 'sbx', true,'a');
        end
        rw.write(temp);
-       rw.close()
-       
-      
+       rw.close()  
 end
+     % save metadata file
      info.nframes = sum(nFrames);
      info.nstacks = nstacks;
      save([mergeFile '.mat'],'info')
